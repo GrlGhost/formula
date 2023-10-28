88ce2cceeeb427fb434c11776853735760c4abcb
Franco B <franco.barravecchia2002@gmail.com>
2023-10-28 17:22:02 -0300

    feat: Variable implement methods

--- src/main/kotlin/edu/austral/ingsis/math/composite/Variable.kt
+++ src/main/kotlin/edu/austral/ingsis/math/composite/Variable.kt
@@ -1,15 +1,12 @@
 package edu.austral.ingsis.math.composite
 
 class Variable(val name: String): Function {
-    override fun toString(): String {
-        TODO("Not yet implemented")
-    }
+    override fun toString(): String = name
+    override fun listVariables(): List<String> = listOf(name)
 
     override fun evaluate(variableValues: Map<String, Number>): Result<Number> {
-        TODO("Not yet implemented")
+        val value: Number = variableValues[name] ?: return Result.failure(Exception("Variable value not defined"))
+        return Result.success(value)
     }
 
-    override fun listVariables(): List<String> {
-        TODO("Not yet implemented")
-    }
 }

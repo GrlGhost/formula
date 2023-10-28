e6732cbd93b4ebb052a17994f4153781c2e7a7fb
Franco B <franco.barravecchia2002@gmail.com>
2023-10-28 17:33:21 -0300

    update: Number toString
    prietier

--- src/main/kotlin/edu/austral/ingsis/math/composite/Number.kt
+++ src/main/kotlin/edu/austral/ingsis/math/composite/Number.kt
@@ -1,7 +1,7 @@
 package edu.austral.ingsis.math.composite
 
 class Number(val value: Double): Function {
-    override fun toString(): String = value.toString()
+    override fun toString(): String = if (value.rem(1) != 0.0) value.toString() else value.toLong().toString()
     override fun evaluate(variableValues: Map<String, Number>): Result<Number> = Result.success(this)
     override fun listVariables(): List<String> = listOf()
 }

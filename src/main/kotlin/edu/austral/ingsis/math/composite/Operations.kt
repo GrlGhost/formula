d7f715a60d7f7d6a5f8472cf30d61b558fab889b
Franco B <franco.barravecchia2002@gmail.com>
2023-10-28 17:38:24 -0300

    feat: operation implement llistVariables()

--- src/main/kotlin/edu/austral/ingsis/math/composite/Operations.kt
+++ src/main/kotlin/edu/austral/ingsis/math/composite/Operations.kt
@@ -3,84 +3,56 @@ package edu.austral.ingsis.math.composite
 
 class Add(val rightFunction: Function, val leftFunction: Function): Function {
     override fun toString(): String = "$rightFunction + $leftFunction"
-
+    override fun listVariables(): List<String> = rightFunction.listVariables() + leftFunction.listVariables()
     override fun evaluate(variableValues: Map<String, Number>): Result<Number> {
-        TODO("Not yet implemented")
-    }
-
-    override fun listVariables(): List<String> {
         TODO("Not yet implemented")
     }
 }
 
 class Subtract(val rightFunction: Function, val leftFunction: Function): Function {
     override fun toString(): String = "$rightFunction - $leftFunction"
-
+    override fun listVariables(): List<String> = rightFunction.listVariables() + leftFunction.listVariables()
     override fun evaluate(variableValues: Map<String, Number>): Result<Number> {
-        TODO("Not yet implemented")
-    }
-
-    override fun listVariables(): List<String> {
         TODO("Not yet implemented")
     }
 }
 
 class Multiply(val rightFunction: Function, val leftFunction: Function): Function {
     override fun toString(): String = "$rightFunction * $leftFunction"
-
+    override fun listVariables(): List<String> = rightFunction.listVariables() + leftFunction.listVariables()
     override fun evaluate(variableValues: Map<String, Number>): Result<Number> {
-        TODO("Not yet implemented")
-    }
-
-    override fun listVariables(): List<String> {
         TODO("Not yet implemented")
     }
 }
 
 class Divide(val rightFunction: Function, val leftFunction: Function): Function {
     override fun toString(): String = "$rightFunction / $leftFunction"
-
+    override fun listVariables(): List<String> = rightFunction.listVariables() + leftFunction.listVariables()
     override fun evaluate(variableValues: Map<String, Number>): Result<Number> {
-        TODO("Not yet implemented")
-    }
-
-    override fun listVariables(): List<String> {
         TODO("Not yet implemented")
     }
 }
 
 class Power(val base: Function, val p: Function): Function {
     override fun toString(): String = "$base ^ $p"
-
+    override fun listVariables(): List<String> = base.listVariables() + p.listVariables()
     override fun evaluate(variableValues: Map<String, Number>): Result<Number> {
-        TODO("Not yet implemented")
-    }
-
-    override fun listVariables(): List<String> {
         TODO("Not yet implemented")
     }
 }
 
 class Root(val base: Function, val p: Function): Function {
     override fun toString(): String = "$base -^ $p"
-
+    override fun listVariables(): List<String> = base.listVariables() + p.listVariables()
     override fun evaluate(variableValues: Map<String, Number>): Result<Number> {
-        TODO("Not yet implemented")
-    }
-
-    override fun listVariables(): List<String> {
         TODO("Not yet implemented")
     }
 }
 
 class Module(val function: Function): Function {
     override fun toString(): String = "|$function|"
-
+    override fun listVariables(): List<String> = function.listVariables()
     override fun evaluate(variableValues: Map<String, Number>): Result<Number> {
-        TODO("Not yet implemented")
-    }
-
-    override fun listVariables(): List<String> {
         TODO("Not yet implemented")
     }
 }
